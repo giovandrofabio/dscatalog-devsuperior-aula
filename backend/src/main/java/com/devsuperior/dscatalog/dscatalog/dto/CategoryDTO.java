c8e31220a649a5882ff6a8a12de6a25ab8998ac1
Giovandro Santos <vsm.giovandro@gmail.com>
2024-05-12 20:06:13 -0300

    DTO

---
+++ backend/src/main/java/com/devsuperior/dscatalog/dscatalog/dto/CategoryDTO.java
@@ -0,0 +1,59 @@
+package com.devsuperior.dscatalog.dscatalog.dto;
+
+import com.devsuperior.dscatalog.dscatalog.entities.Category;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class CategoryDTO implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+
+    private Long id;
+    private String name;
+
+    public CategoryDTO() {
+    }
+
+    public CategoryDTO(Category entity) {
+        this.id = entity.getId();
+        this.name = entity.getName();
+    }
+
+    public CategoryDTO(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        CategoryDTO that = (CategoryDTO) o;
+
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+}
--- backend/src/main/java/com/devsuperior/dscatalog/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/dscatalog/services/CategoryService.java
@@ -1,11 +1,13 @@
 package com.devsuperior.dscatalog.dscatalog.services;
 
+import com.devsuperior.dscatalog.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.dscatalog.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CategoryService {
@@ -13,7 +15,9 @@ public class CategoryService {
     @Autowired
     private CategoryRepository repository;
 
-    public List<Category> findAll() {
-        return repository.findAll();
+    public List<CategoryDTO> findAll() {
+        List<Category> list = repository.findAll();
+
+        return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
     }
 }
