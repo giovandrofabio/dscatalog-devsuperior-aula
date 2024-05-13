0528b82f7678305199c199d56b34c50166911366
Giovandro Santos <vsm.giovandro@gmail.com>
2024-05-13 06:50:40 -0300

    Product repository, service, resource, GET Ok

--- backend/src/main/java/com/devsuperior/dscatalog/dscatalog/dto/CategoryDTO.java
+++ backend/src/main/java/com/devsuperior/dscatalog/dscatalog/dto/CategoryDTO.java
@@ -8,8 +8,6 @@ import java.util.Objects;
 public class CategoryDTO implements Serializable {
 
     private static final long serialVersionUID = 1L;
-
-
     private Long id;
     private String name;
 
